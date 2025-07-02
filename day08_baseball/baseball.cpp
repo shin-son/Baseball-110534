6f8d202e9a4e4d61f53e7591a406a234763f6c75
shin.son <thstls110534@naver.com>
2025-07-02 14:20:53 +0900

    [refactoring] Try 01

--- day08_baseball/baseball.cpp
+++ day08_baseball/baseball.cpp
@@ -20,6 +20,10 @@ public:
 			|| guessNumber[1] == guessNumber[2];
 	}
 
+	bool isEqualNumber(const string& guessNumber, int number) {
+		return guessNumber[number] == question[number];
+	}
+
 	void assertIllegalArgument(const string& guessNumber) {
 		if (guessNumber.length() != 3) {
 			throw length_error("Must be three letters.");
@@ -43,21 +47,12 @@ public:
 			return { true , 3, 0 };
 		}
 		else {
-			if (guessNumber[0] == question[0])
-				result.strikes++;
-			else if (guessNumber[0] == question[1] || guessNumber[0] == question[2])
-				result.balls++;
-
-			if (guessNumber[1] == question[1])
-				result.strikes++;
-			else if (guessNumber[1] == question[0] || guessNumber[1] == question[2])
-				result.balls++;
-
-
-			if (guessNumber[2] == question[2])
-				result.strikes++;
-			else if (guessNumber[2] == question[0] || guessNumber[2] == question[1])
-				result.balls++;
+			for (int number = 0; number < 3; ++number) {
+				if (isEqualNumber(guessNumber, number))
+					result.strikes++;
+				else if (guessNumber[number] == question[(number + 1) % 3] || guessNumber[(number + 1) % 3] == question[2])
+					result.balls++;			
+			}
 		}
 		return result;
 	}
