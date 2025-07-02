429ad6b32dca5b1c5cc0ca674df62cbd81a3c144
shin.son <thstls110534@naver.com>
2025-07-02 13:46:08 +0900

    [feature] 04

--- day08_baseball/baseball.cpp
+++ day08_baseball/baseball.cpp
@@ -1,20 +1,32 @@
 #include <stdexcept>
 
 using namespace std;
+
+struct GuessResult {
+	bool solved;
+	int strikes;
+	int balls;
+};
+
 class Baseball {
 public:
-	void guess(const string& guessNumber) {
-		assertIllegalArgument(guessNumber);
+	explicit Baseball(const string& question) 
+		: question(question) {
+	}
+	
+	bool isDuplicatedNumber(const string& guessNumber) {
+		return guessNumber[0] == guessNumber[1]
+			|| guessNumber[0] == guessNumber[2]
+			|| guessNumber[1] == guessNumber[2];
 	}
 
-private:
 	void assertIllegalArgument(const string& guessNumber) {
 		if (guessNumber.length() != 3) {
 			throw length_error("Must be three letters.");
 		}
 
 		for (char ch : guessNumber) {
-			if (ch < '0' || ch > '9') continue;
+			if (ch >= '0' && ch <= '9') continue;
 			throw invalid_argument("Must be number");
 		}
 
@@ -23,9 +35,11 @@ private:
 		}
 	}
 
-	bool isDuplicatedNumber(const string& guessNumber) {
-		return (guessNumber[0] == guessNumber[1]
-			|| guessNumber[0] == guessNumber[2]
-			|| guessNumber[1] == guessNumber[2]);
+	GuessResult guess(const string& guessNumber) {
+		assertIllegalArgument(guessNumber);
+		return { true , 3, 0 };
 	}
+
+private:
+	string question;
 };
--- day08_baseball/baseball_test.cpp
+++ day08_baseball/baseball_test.cpp
@@ -3,23 +3,30 @@
 
 class BaseballFixture : public testing::Test {
 public:
-	Baseball game;
+	Baseball game{ "123" };
 	void assertIllegalArgument(string guessNumber) {
 		try {
 			game.guess(guessNumber);
 			FAIL();
 		}
-		catch (exception e) {
+		catch (const exception& e) {
 			//PASS
 		}
 	}
 };
 
-
 TEST_F(BaseballFixture, ThrowExceptionWhenInvalidCase) {
 	assertIllegalArgument("12");
 	assertIllegalArgument("12s");
 	assertIllegalArgument("121");
+}
+
+TEST_F(BaseballFixture, ReturnSolvedResultIfMatchedNumber) {
+	GuessResult result = game.guess("123");
+
+	EXPECT_TRUE(result.solved);
+	EXPECT_EQ(3, result.strikes);
+	EXPECT_EQ(0, result.balls);
 }
 
 int main() {
