ef78829e8a10497fd5ecb6fa35d8290c95901d5f
shin.son <thstls110534@naver.com>
2025-07-02 13:13:40 +0900

    [feature] 02

--- day08_baseball/baseball_test.cpp
+++ day08_baseball/baseball_test.cpp
@@ -1,9 +1,14 @@
 #include "gmock/gmock.h"
 #include "baseball.cpp"
 
-TEST(BaseballGame, TryGameTest) {
+TEST(BaseballGame, ThrowExceptionWhenInputLengthIsUnmached) {
 	Baseball game;
 	EXPECT_THROW(game.guess(string("12")), length_error);
+}
+
+TEST(BaseballGame, ThrowExceptionWhenInvalidChar) {
+	Baseball game;
+	EXPECT_THROW(game.guess(string("12s")), invalid_argument);
 }
 
 int main() {
